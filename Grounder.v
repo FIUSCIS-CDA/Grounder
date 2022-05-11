c7cd5a736b5a94785d21136cf9013f8f19ba2505
movingpictures83 <movingpictures83@gmail.com>
2022-05-11 14:04:05 -0400

    Updated testbench for new format

--- Grounder.v
+++ Grounder.v
@@ -15,7 +15,7 @@
 
 // PROGRAM		"Quartus Prime"
 // VERSION		"Version 20.1.1 Build 720 11/11/2020 SJ Lite Edition"
-// CREATED		"Tue May 10 09:13:53 2022"
+// CREATED		"Tue May 10 09:19:44 2022"
 
 module Grounder(
 	Input_To_Ground
