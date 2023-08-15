6aebf725d65fba29c29d33a8bf97e09732666f61
Trevor Cickovski <movingpictures83@gmail.com>
2023-08-15 10:40:47 -0400

    Upgraded Quartus

--- Grounder.v
+++ Grounder.v
@@ -15,7 +15,7 @@
 
 // PROGRAM		"Quartus Prime"
 // VERSION		"Version 20.1.1 Build 720 11/11/2020 SJ Lite Edition"
-// CREATED		"Tue May 10 09:19:44 2022"
+// CREATED		"Tue Aug 15 10:39:26 2023"
 
 module Grounder(
 	Input_To_Ground
